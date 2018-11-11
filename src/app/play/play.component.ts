2f0fe96a6f5a58a32b7deb3dd7265b5680fb5c36
[hnakae] <[nakae27@gmail.com]>
2018-11-11 12:25:18 -0800

    jquery fix

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -10,25 +10,6 @@ export class PlayComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
   }
-  var move = 1;
-  var play = true;
-
-  $("div.inner").click(function () {
-    if ($(this).text() == "" && play) {
-      if ((move % 2) == 1) {
-        $(this).toggleClass("black");
-      }
-      else {
-        $(this).toggleClass("white");
-      }
-      move++;
-    }
-  });
-
-  $(".reset").click(function () {
-    location.reload();
-  });
-
+  
 }
