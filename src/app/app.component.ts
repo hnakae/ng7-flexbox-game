3123b49de883f28e866e0f7461390a25f5762682
[hnakae] <[nakae27@gmail.com]>
2018-11-13 19:23:51 -0800

    fixed jquery on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -8,51 +7,4 @@ import * as $ from 'jquery';
 })
 export class AppComponent {
   title = 'ng7-flexbox';
-
-  public ngOnInit(){
-    $(document).ready(function () {
-
-      var move = 1;
-      var play = true;
-
-      $("div.inner").click(function () {
-        if ($(this).text() == "" && play) {
-          if ((move % 2) == 1) {
-            $(this).toggleClass("black");
-          }
-          else {
-            $(this).toggleClass("white");
-          }
-          move++;
-        }
-      });
-
-      $(".reset").click(function () {
-        location.reload();
-      });
-
-      $(".changebgc").click(function () {
-        var myColorsArray = ["turquoise", "pink", "lightgray"];
-        var tileColorsArray = ["cyan", "darkcyan", "bluegreen"];
-        var arrayLength = myColorsArray.length;
-
-        var rand = myColorsArray[Math.floor(Math.random() * arrayLength)];
-        var tilerand = tileColorsArray[Math.floor(Math.random() * arrayLength)];
-
-        $("body").css("background", rand);
-        $(".box").css("background", tilerand);
-
-
-      });
-
-    });
-
-    $(function () {
-      $(document).scroll(function () {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-      })
-    })
-  }
-
 }
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-play',
@@ -10,6 +11,47 @@ export class PlayComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+
+      var move = 1;
+      var play = true;
+
+      $("div.inner").click(function () {
+        if ($(this).text() == "" && play) {
+          if ((move % 2) == 1) {
+            $(this).toggleClass("black");
+          }
+          else {
+            $(this).toggleClass("white");
+          }
+          move++;
+        }
+      });
+
+      $(".reset").click(function () {
+        location.reload();
+      });
+
+      $(".changebgc").click(function () {
+        var myColorsArray = ["turquoise", "pink", "lightgray"];
+        var tileColorsArray = ["cyan", "darkcyan", "bluegreen"];
+        var arrayLength = myColorsArray.length;
+
+        var rand = myColorsArray[Math.floor(Math.random() * arrayLength)];
+        var tilerand = tileColorsArray[Math.floor(Math.random() * arrayLength)];
+
+        $("body").css("background", rand);
+        $(".box").css("background", tilerand);
+      });
+
+    });
+
+    $(function () {
+      $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+      })
+    })
   }
   
 }
